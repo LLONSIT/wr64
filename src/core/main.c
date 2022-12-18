a5adc62d50d7dc8bdbdb6b3117e3183ebfdfb3e2
LLONSIT <lonsitsyt456@gmail.com>
2022-12-18 01:12:05 -0500

    Documenting code, and extracting libultra segs

---
+++ src/core/main.c
@@ -0,0 +1,17 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/core/main/func_80047470.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/core/main/func_800474A0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/core/main/func_800474E4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/core/main/func_80047530.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/core/main/func_8004791C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/core/main/func_80047AA4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/core/main/func_80047B00.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/core/main/func_80047B20.s")
