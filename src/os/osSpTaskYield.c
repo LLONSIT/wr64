51f7869f94e7acc21b1cb441afdde2f0dc8d8e41
LLONSIT <lonsitsyt456@gmail.com>
2022-12-19 01:14:46 -0500

    fixing the build

--- src/os/osSpTaskYield.c
+++ src/os/osSpTaskYield.c
@@ -4,6 +4,6 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/os/osSpTaskYield/func_800C6040.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/os/osSpTaskYield/func_800C615C.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/os/osSpTaskYield/osSpTaskLoad.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/os/osSpTaskYield/func_800C62BC.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/os/osSpTaskYield/osSpTaskStartGo.s")
