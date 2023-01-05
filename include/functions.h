c4320303756e5aecf01b8afc6218d3e3342a7fdc
LLONSIT <lonsitsyt456@gmail.com>
2023-01-04 22:46:48 -0500

    Documenting and matching some code, mio0 segs soon!

--- include/functions.h
+++ include/functions.h
@@ -3,6 +3,9 @@
 
 //0x800B
 extern void func_800BF370(void);
+void func_800AF43C(f32, f32, f32);
+void func_800B4788(s32 source_id, f32 x_position, f32 y_position, f32 z_position, f32 x_velocity, f32 y_velocity, f32 z_velocity);
+
 
 //0x8004
 extern void func_8004A2B4(void); 
--- src/game_6EF50.c
+++ src/game_6EF50.c
@@ -1,13 +1,21 @@
 #include "common.h"
 
-//TODO 
 
 #pragma GLOBAL_ASM("asm/nonmatchings/game_6EF50/func_800B4750.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/game_6EF50/func_800B4788.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/game_6EF50/func_800B490C.s")
 
+//TODO: clarify more this
+void create_obj(f32 source_id, f32 x_position, f32 y_position, f32 z_position, f32 x_velocity, f32 y_velocity) {
+    s32 index;
+                //Setting some objs
+    for(index = 0 ; index != 8 ; index++) {
+        func_800B4788(index, source_id, x_position, y_position, z_position, x_velocity, y_velocity);
+
+   }
+    func_800AF43C(source_id, x_position + 24.0f, y_position);
+}
 #pragma GLOBAL_ASM("asm/nonmatchings/game_6EF50/func_800B49C4.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/game_6EF50/func_800B4ABC.s")
