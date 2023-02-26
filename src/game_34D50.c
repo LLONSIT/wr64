a50725b5b13a2ae6ac076e1b14ba99727b7e771e
LLONSIT <lonsitsyt456@gmail.com>
2023-02-25 22:29:52 -0500

    Some libultra segs

--- src/game_34D50.c
+++ src/game_34D50.c
@@ -1,6 +1,24 @@
-#include "common.h"
+#include <ultra64.h>
+#include "variables.h"
+#include "structs.h"
+#include "functions.h"
+#include "macros.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/game_34D50/func_8007A550.s")
+void func_8007A550(Gfx** gDisplayList) {
+
+    Gfx *gDisplayListHead = *gDisplayList;
+
+    gDPPipeSync(gDisplayListHead++);
+    gDPSetCycleType(gDisplayListHead++, G_CYC_FILL);
+    gDPSetFillColor(gDisplayListHead++, 0x00010001);
+    gDPPipeSync(gDisplayListHead++);
+    gDPFillRectangle(gDisplayListHead++, 0, 0, 319, 239);
+    gDPPipeSync(gDisplayListHead++);
+    gDPSetCycleType(gDisplayListHead++, G_CYC_1CYCLE);
+
+    *gDisplayList = gDisplayListHead;
+
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/game_34D50/func_8007A5D4.s")
 
